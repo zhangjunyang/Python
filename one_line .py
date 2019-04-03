07b06cf094b24bb1e2d0daeba2c1f7f91984b2db
zhangjunyang <949625150@qq.com>
2019-04-03 18:28:20 +0800

    Update one_line .py

--- one_line .py
+++ one_line .py
@@ -2,7 +2,7 @@
 print(''.join(__import__('random').choice('\u2571\u2572') for i in range(50*24)))
 
 #一行打印桃心
-print(' '.join([''.join([('AndyLove'[(x-y)%8]if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
+print(' '.join([''.join([('AndyLove'[(x-y)%8] if((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3<=0 else' ')for x in range(-30,30)])for y in range(15,-15,-1)]))
 
 #一行输出九九乘法表
 print (' '.join([' '.join(['%s*%s=%-2s' % (y,x,x*y) for y in range(1,x+1)]) for x in range(1,10)]))
